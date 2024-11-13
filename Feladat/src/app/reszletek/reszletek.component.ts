ac1e03131d9e4c2278b81ac447ec37eac3815f63
bandiiskola <125878169+bandiiskola@users.noreply.github.com>
2024-11-13 10:54:46 +0100

    létrehozva

---
+++ Feladat/src/app/reszletek/reszletek.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reszletek',
+  standalone: true,
+  imports: [],
+  templateUrl: './reszletek.component.html',
+  styleUrl: './reszletek.component.css'
+})
+export class ReszletekComponent {
+
+}
